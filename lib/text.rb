c50545248da67c576071b9abd0c2e2c74be538a7
Alex Psitos <apsitos08@gmail.com>
2022-03-06 22:51:25 -0500

    Write test for dissect method

--- lib/text.rb
+++ lib/text.rb
@@ -2,12 +2,26 @@
 
 class Text
 
-  def process(input)
+  # def render(input, line_length = 80)
+  #   until input.empty? do
+  #     line = input.slice(0..1)
+  #   end
+  # end
+  # def render(input)
+  #   array = ["", "", ""]
+  #   require 'pry'; binding.pry
+  #   input.each do |letter|
+  #     letter.each do |line|
+  #       array[0] << line[0]
+  #       array[1] <<
+  #     end
+  #   end
+  # end
 
-  end
   # def translate(incoming_braille)
   #   output = []
   #   braille_message_line_length = 80
   #
   #   until incoming_braille.empty? do
-  #     line = incoming_braille.each do 
+  #     line = incoming_braille.each do
+end
--- spec/text_spec.rb
+++ spec/text_spec.rb
@@ -3,77 +3,117 @@ require './lib/text'
 
 
 RSpec.describe Text do
-  describe "#translate" do
 
-    it "turns a single braille character into a single text character" do
-      incoming_braille = [
-        [
-          [
-            ["0", "."],
-            [".", "."],
-            [".", "."],
-          ],
-        ]
-      ]
-      expected = "a"
-      expect(subject.translate(incoming_braille)).to eq(expected)
-    end
+  describe "dissect" do
+    it "takes a string of plaintext and converts it to nested arrays of two characters each" do
+      input = "0.00.."
+      output = [["0."], ["00"], [".."]]
+      expect(subject.dissect(input)).to eq(output)
   end
 
-  describe "#render" do
-    it "takes an array of strings and turns it into a braille array" do
-      input = [
-        [
-          "0.0.",
-          "....",
-          "....",
-        ],
-        [
-          "0.0.",
-          "....",
-          "....",
-        ],
-        [
-          "0.",
-          "..",
-          "..",
-        ],
-      ]
+  # describe "#braille_text_to_braille_array" do
+  #   it "takes a single letter of braille in text form and converts it to a braille letter array" do
+  #     input = "0.00.."
+  #     output =
+  #   end
+  # end
 
-      output = [
-        [ #line 1
-          [
-            ["0", "."],
-            [".", "."],
-            [".", "."],
-          ],
-          [
-            ["0", "."],
-            [".", "."],
-            [".", "."],
-          ],
-        ],
-        [ #line 2
-          [
-            ["0", "."],
-            [".", "."],
-            [".", "."],
-          ],
-          [
-            ["0", "."],
-            [".", "."],
-            [".", "."],
-          ],
-        ],
-        [ #line 3
-          [
-            ["0", "."],
-            [".", "."],
-            [".", "."],
-          ],
-        ],
-      ]
-      expect(subject.render(input)).to eq(output)
-    end
-  end
+  # describe "#render" do
+  #
+  #   it "takes a single braille letter and turns it into a braille array" do
+  #     input =
+  #     [
+  #       [
+  #         "0.0.",
+  #         "....",
+  #         "....",
+  #       ]
+  #     ]
+  #
+  #     output =
+  #     [
+  #       [
+  #         ["0", "."],
+  #         [".", "."],
+  #         [".", "."],
+  #       ]
+  #     ]
+
+  #     expect(subject.render(input)).to eq(output)
+  #   end
+  # end
+
+  #   xit "takes an array of strings and turns it into a braille array" do
+  #     input = [
+  #       [
+  #         "0.0.",
+  #         "....",
+  #         "....",
+  #       ],
+  #       [
+  #         "0.0.",
+  #         "....",
+  #         "....",
+  #       ],
+  #       [
+  #         "0.",
+  #         "..",
+  #         "..",
+  #       ],
+  #     ]
+  #
+  #     output = [
+  #       [ #line 1
+  #         [
+  #           ["0", "."],
+  #           [".", "."],
+  #           [".", "."],
+  #         ],
+  #         [
+  #           ["0", "."],
+  #           [".", "."],
+  #           [".", "."],
+  #         ],
+  #       ],
+  #       [ #line 2
+  #         [
+  #           ["0", "."],
+  #           [".", "."],
+  #           [".", "."],
+  #         ],
+  #         [
+  #           ["0", "."],
+  #           [".", "."],
+  #           [".", "."],
+  #         ],
+  #       ],
+  #       [ #line 3
+  #         [
+  #           ["0", "."],
+  #           [".", "."],
+  #           [".", "."],
+  #         ],
+  #       ],
+  #     ]
+  #    # require 'pry'; binding.pry
+  #     expect(subject.render(input)).to eq(output)
+  #   end
+  # end
+  #
+  # describe "#translate" do
+  #
+  #   xit "turns a single braille character into a single text character" do
+  #     incoming_braille = [
+  #       [
+  #         [
+  #           ["0", "."],
+  #           [".", "."],
+  #           [".", "."],
+  #         ],
+  #       ]
+  #     ]
+  #     expected = "a"
+  #     expect(subject.translate(incoming_braille)).to eq(expected)
+  #   end
+  # end
 end
