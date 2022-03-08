2bada8ddd669b633ad69fa8dc36914800e1725c3
Alex Psitos <apsitos08@gmail.com>
2022-03-07 21:18:45 -0500

    Test: reformat rows to braille test to make more logical/visual sense

--- lib/text.rb
+++ lib/text.rb
@@ -27,19 +27,18 @@ class Text
     # require 'pry'; binding.pry
     stacked_lines = []
     until stacked_rows.length == 0 do
-      stacked_lines << stacked_rows.shift(3)
+      stacked_lines << stacked_rows.shift(3) #grouping by line
     end
     new_array = []
     result = stacked_lines.each do |line|
 
-      row_1 = line[0]
+      row_1 = line[0] #take the first items from arrays in 102, 103, 104 and stick them together
       row_2 = line[1]
       row_3 = line[2]
       raw_braille_chars = row_1.zip(row_2, row_3)
-      # require 'pry'; binding.pry
       braille_chars = raw_braille_chars.map do |raw_char|
         braille_char = raw_char.map do |char_piece|
-          char_piece.chars
+          char_piece.chars #could refactor to make a rows to braille and then raw to format: 41-43
         end
         new_array << braille_char
       end
